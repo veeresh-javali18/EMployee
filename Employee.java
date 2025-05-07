e2e88c7736b513b2544e86c059c761e214b39840
veeresh-javali18 <veereshjavali038@gmail.com>
2025-05-07 13:55:10 +0530

    first commit

---
+++ Employee.java
@@ -0,0 +1,24 @@
+public class Employee {
+    String name;
+    int id;
+    double salary;
+    public Employee(String name,int id, double salary)
+    {
+        this.name=name;
+        this.id=id;
+        this.salary=salary;
+    }
+    public void displayInfo(){
+        System.out.println("Employee Id: "+ id);
+        System.out.println("Employee Name: "+ name);
+        System.out.println("Salary : "+ salary);
+        
+    }
+    public static void main(String[]args)
+    {
+        Employee emp1=new Employee("XYZ",101,50000.0);
+        Employee emp2=new Employee("ABC",102,50000.0);
+        emp1.displayInfo();
+        emp2.displayInfo();
+    }
+}
